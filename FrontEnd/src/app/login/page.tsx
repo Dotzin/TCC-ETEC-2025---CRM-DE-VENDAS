ed5a0d6882679946b7dc85267966d7cdff1bb479
DOTzin <augustoeliasmmribas@gmail.com>
2025-05-22 12:37:20 -0300

    Base Dos Aqruivos e correção de erros

---
+++ FrontEnd/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+// page.tsx
+import React from 'react';
+
+const Page: React.FC = () => {
+  return (
+    <div>
+      <h1>Bem-vindo à minha página!</h1>
+      <p>Este é um exemplo simples de um arquivo page.tsx em um aplicativo React com TypeScript.</p>
+    </div>
+  );
+};
+
+export default Page;
